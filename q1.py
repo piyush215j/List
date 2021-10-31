dd15250432c722f0edf0ed47e9ba320bcc02fd60
Piyush Dwivedi <72141362+piyush215j@users.noreply.github.com>
2021-10-31 10:45:08 +0530

    Update q1.py
    for arrays

--- q1.py
+++ q1.py
@@ -1,13 +1,8 @@
-l=[]
-n=int(input("Enter List size:"))
-for i in range(n):
-    l.append(int(input()))
-l.sort()
-s=int(input("Enter the element to be inserted"))
-i=0
-while i<len(l):
-    if s<l[i]:
-        break
-    i=i+1
-l.insert(i,s)
-print(l)
+#Python program to add all the array elements using the built-in function
+lst = []
+num = int(input("Enter the size of the array: "))
+print("Enter array elements: ")
+for n in range(num):
+  numbers = int(input())
+  lst.append(numbers)
+print("Sum:", sum(lst))
